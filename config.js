24c1f0937636757800f8bf27fee4c71a51c16bbe
Aayush Shah <aayushshah2597@gmail.com>
2022-04-14 23:03:31 -0600

    Backend APIs for Frontend

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+  dbUrl:
+    "mongodb+srv://backendEngineer:TwitteranalysisFOSE@twitteranalysis.dp3pm.mongodb.net/twitter-analysis?retryWrites=true&w=majority",
+};
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Twitter = require("twitter");
+const mongoose = require("mongoose");
+const config = require("./config");
+const validator = require("email-validator");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const apiKey = "TzvKlgFTYI8j79S42QVYe0ZDE";
+const apiSecretKey = "4xx3WknZVEjphlyOKJiyCngmsKkfEKVLV3H2uYpUEmE3vGRbOc";
+const accessToken = "787976257057263617-G0v02VJsj4SjjOMCLWtrhz5npLS75T6";
+const accessTokenSecret = "Rj3ZXGYk8Jp8MyS1Zm5kZvvZkJAe0XJY29TCpQQFtb2GH";
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  email: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  fullName: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+let userHistorySchema = new Schema({
+  username: {
+    type: String,
+  },
+  history: {
+    type: Array,
+  },
+});
+
+let currentUserSchema = new Schema(
+  {
+    username: {
+      type: String,
+    },
+    currentActivity: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+let userModel = mongoose.model("users", userSchema);
+let userHistoryModel = mongoose.model(
+  "user-twituser-history",
+  userHistorySchema
+);
+let currentUserModel = mongoose.model("active-current-user", currentUserSchema);
+
+const dbUrl = config.dbUrl;
+
+var options = {
+  keepAlive: true,
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbUrl, options, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+var client = new Twitter({
+  consumer_key: apiKey,
+  consumer_secret: apiSecretKey,
+  access_token_key: accessToken,
+  access_token_secret: accessTokenSecret,
+});
+
+app.post("/getTweets", (req, res) => {
+  var params = { screen_name: req.body.username };
+  client.get(
+    "statuses/user_timeline",
+    params,
+    function (error, tweets, response) {
+      if (!error) {
+        res.json(tweets);
+      } else {
+        res.status(500);
+      }
+    }
+  );
+});
+
+app.post("/signup", (req, res) => {
+  if (req) {
+    let newUser = new userModel(req.body);
+    newUser
+      .save()
+      .then((doc) => {
+        console.log(doc);
+        res.send("Logging Successful");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send("Error in mongoDB");
+        res.status(500);
+      });
+  } else {
+    res.send("Error in input data");
+  }
+});
+
+app.post("/login", (req, res) => {
+  if (req) {
+    if (validator.validate(req.body.userCred)) {
+      userModel
+        .findOne({ email: req.body.userCred })
+        .then((doc) => {
+          console.log(doc.password);
+          if (doc.password === req.body.userPassword) {
+            res.send("Logging Successful");
+          } else {
+            res.send("Password Incorrect");
+          }
+        })
+        .catch((err) => {
+          res.send("Email incorrect");
+        });
+    } else {
+      userModel
+        .findOne({ username: req.body.userCred })
+        .then((doc) => {
+          console.log(doc.password);
+          if (doc.password === req.body.userPassword) {
+            res.send("Logging Successful");
+          } else {
+            res.send("Password Incorrect");
+          }
+        })
+        .catch((err) => {
+          res.send("Username incorrect");
+        });
+    }
+  }
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("Server is Running and Healthy on port " + port);
+});
