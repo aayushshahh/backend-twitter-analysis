054c52dcfcbf87a4dbc088f1e0f8d8f538f55cae
YOUR_USERNAME <89421055+chaitanyaarepalli47@users.noreply.github.com>
2022-04-23 20:07:52 -0600

    added a test suite

---
+++ test/test.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const chai = require('chai');
+const request = require('supertest');
+
+describe('POST get tweets',() => {
+    it('should get tweets and expect a response', () => {
+        
+    });
+    it('should throw an error for invalid username', () =>{
+
+    });
+});
+
+describe('POST sign-up',() => {
+    it('Logging successful', () => {
+        
+    });
+    it('error in mongoDB', () =>{
+
+    });
+    it('error in input data', () =>{
+
+    });
+});
+
+describe('login', () => {
+    
+});
+
+describe('add History',() => {
+
+});
+
+describe('get History', () =>
+{
+    
+});
